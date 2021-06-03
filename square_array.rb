f45390390e88c72c2b1039fbb549988dc3b0f7b8
Mike <mikewrubel51@gmail.com>
2021-06-03 09:09:55 -0400

    commit tree d1cda6dfb76010774395b07885b9b44e758dc681

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
   # your code here
+  
+  array.each do |num|
+    squared_Array.unshift = num * num
+    puts squared_Array
+      squared_Array.each do |numbers|
+        second_Squared_Array.unshift = numbers * numbers
+        puts second_Squared_Array
+    end
+  end
 end
